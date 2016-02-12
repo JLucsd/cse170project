e5aadf76bcf1ead73b5b9233bc1d5c6079bb4a49
vagrant <vagrant@precise32.(none)>
2016-02-12 20:10:49 +0000

    ok

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,7 +1,16 @@
 // Get all of our friend data
+var time_data = require('../data.json');
 var data = require('../ralphs-userlist.json');
 
+
 exports.view = function(req, res){
+	console.log("inside");
+	if(req.query.description > 0){
+	var newFriend = JSON.parse('{ "name": "' +
+	req.query.name+'", "description": "'+
+	req.query.description+'", "imageURL": "'+
+	'http://lorempixel.com/500/500/people"}');
+	time_data["friends"].push(newFriend);}
 	res.render('homepage',data);
-	console.log(data);
+	console.log(time_data);
 };
