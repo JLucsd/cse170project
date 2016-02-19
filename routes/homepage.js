fd00dfd2c3e288eff92d0bee3ac6cf3efd1b49b8
ruchikas <ruchikas300@gmail.com>
2016-02-18 19:26:33 -0800

    changes to add lsit

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,7 +4,6 @@ var data = require('../store.json');
 var store_id = 0;
 
 exports.view = function(req, res){
-	
 
 	if(req.query.description > 0){
 		var newFriend = JSON.parse('{ "name": "' +
@@ -12,6 +11,22 @@ exports.view = function(req, res){
 			req.query.description+'", "imageURL": "'+'http://lorempixel.com/500/500/people"}');
 		time_data["friends"].push(newFriend);
 	}
-	res.render('homepage',data,store_id);
+	res.render('homepage',data);
 	
+
+	var title = req.query.title;
+	var date = req.query.month + ","+req.query.date+" 2016";
+	var items = req.query.item;
+	 newList = {
+				"id": "list3",
+				"title":title,
+				"date": date,	
+				"items":items					
+				};
+	console.log(newList);
+
+	var val = data['stores'];
+	//for(i)
+	var val2 = val[0];
+	val2['lists'].push(newList);
 };
