35924452031b7c051667fd911d324c628ddae5a2
vagrant <vagrant@precise32.(none)>
2016-02-26 08:03:53 +0000

    Revert "Revert "new""
    This reverts commit 61fdf55bf8adad976ef218ae8ce7c4d795cd9a93.

--- routes/editlist.js
+++ routes/editlist.js
@@ -10,10 +10,8 @@ exports.view = function(req, res){
 	
 	//res.send(id,sid);
 	console.log(data);
-	console.log(req.query.items);																										
-	res.render('editlist',data);
-	
-	if(req.query.items != undefined){		
+	console.log(req.query.item);																										
+	/*if(req.query.item != undefined){		
 		var items = req.query.item;
 		console.log("Items=" + items);
 		 newList = {
@@ -28,6 +26,9 @@ exports.view = function(req, res){
 		var val2 = val[0];
 		val2['lists'].push(newList);
 		console.log("new json= " + data);
-		}
+		}*/
+	res.render('editlist',data);
+	
+	
 	
 };
