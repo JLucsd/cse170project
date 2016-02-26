9470f71f80bdf01bbba8db7924a173891582234c
vagrant <vagrant@precise32.(none)>
2016-02-26 06:21:40 +0000

    asd

--- routes/homepage.js
+++ routes/homepage.js
@@ -15,10 +15,16 @@ exports.view = function(req, res){
 	console.log("check store-num before" +data);
 	//var val = data['stores'];
 	data["store_num"] = Number(0);
-	console.log(data);
+	data["ralps_num"]=true;
+	//console.log(data);
+	console.log("xxxxxxxxxxxxxxxx");
+	console.log(req.query.item);
 	res.render('homepage',data);
-	
+	console.log(req.query.edit_ok);
+	if(req.query.edit_ok == undefined)
 	//console.log(" title= "+req.query.title)
+	{
+		console.log(req.query.edit_ok);
 	if(req.query.title != undefined){
 		var title = req.query.title;
 		var date = req.query.month + ","+req.query.date+" 2016";
@@ -39,4 +45,29 @@ exports.view = function(req, res){
 		val2['lists'].push(newList);
 		console.log("\nnew json= \n" + data);
 		}
+	}
+	else
+
+	{
+		console.log("ya man!!");
+		var val = data['stores'];
+		var val2= val[0];
+		var val3 = val2['lists'];
+		for(i=0;i<val3.length;i++)
+		{
+			var vall = val3[i];
+			console.log("prnt");
+			console.log(vall['id']);
+			console.log(req.query.list_id)
+			if(vall["id"] == req.query.list_id)
+			{
+				console.log("idididid");
+				var items = req.query.item;
+				var it_list = items.split(",");
+				
+				vall["items"] = it_list;
+			}
+		}
+
+	}
 };
